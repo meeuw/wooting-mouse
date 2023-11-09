8e1c71624393b2f09d93d5960fa97286ff642f7c
Dick Marinus <dick@mrns.nl>
2023-11-09 20:43:11 +0100

    update python

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-sudo /usr/local/venv/wooting-mouse-3.11/bin/pip install /home/$USER/git/wooting-mouse/
+sudo /usr/local/venv/wooting-mouse-3.12/bin/pip install /home/$USER/git/wooting-mouse/
 cd /usr/local/venv
-sudo tar cf /home/$USER/wooting-mouse-3.11.tar wooting-mouse-3.11
+sudo tar cf /home/$USER/wooting-mouse-3.12.tar wooting-mouse-3.12
